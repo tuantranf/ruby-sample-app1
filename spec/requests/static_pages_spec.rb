2f780f8b931c0cb697b19333fc84da39f5ab3393
Tran Minh Tuan <tuantranf@gmail.com>
2015-03-22 16:07:10 +0900

    Added static pages & test.

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  describe "Home Page" do
+
+    it "GET /static_pages" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get static_pages_home_path
+      response.status.should be(200)
+    end
+
+  	it "should have the content 'Sample App'" do
+  		visit '/static_pages/home'
+  		expect(page).to have_content('Sample App')
+  	end
+  end
+
+  describe "Help Page" do
+
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+
+  end
+
+  describe "About Page" do
+
+  	it "should have the content 'About Us'" do
+  		visit '/static_pages/about'
+  		expect(page).to have_content('About Us')
+  	end
+
+  end
+
+end
