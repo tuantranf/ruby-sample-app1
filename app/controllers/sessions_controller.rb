51741dfe70a1bfcfa198e3070c60397733964093
Tran Minh Tuan <tuantranf@gmail.com>
2015-03-31 01:23:07 +0900

    Refactored sign in page using form_tag.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
       sign_in user
-      redirect_to user
+      redirect_to user, :notice => "Logged in!"
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     sign_out
-    redirect_to root_url
+    redirect_to root_url, :notice => "Logged out!"
   end
 
 end
