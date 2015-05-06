b22b532de1ade9c6d53d0e6a00ddc0c34aaf1c64
Tran Minh Tuan <tuantranf@gmail.com>
2015-05-07 00:15:57 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -104,4 +104,20 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 
+  it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
+
 end
